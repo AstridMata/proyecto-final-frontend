6b9d7bd66a32a33a04277696a3e3d5e5560d4537
LOREN932 <lorenamoujalli860@gmail.com>
2021-12-04 15:50:45 +0000

    eventos-login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,54 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import react, {useState} from 'react';
+import {Redirect} from 'react-router';
+import axios from 'axios';
+
 const Form = () => {
+    const [email, setEmail]= useState('');
+const [password,setPassword]= useState ('');
+const [auth,setAuth]= useState(false);
+
+function handleSubmit(e) {
+    e.preventDefault()
+    console.log(email,password);
+
+    axios.post('http://localhost:8080/setting/login',{
+        email:email,
+        password:password
+        })
+        .then(function(response){
+        if(response.status ===200){
+        setAuth(true)
+        localStorage.setItem('token',response.data.token)
+        }
+        console.log(response);
+        })
+        .catch(function(error){
+        console.log(error);
+        });
+        
+}    
+
 
     return ( 
-        
+        <>
+        {auth?<Redirect to='/Details'/>:
         <div className="login-box"> 
             <h1>logo</h1>
-         <form>
+         <form onSubmit={handleSubmit}>
         <label>Email </label>
-        <input type="email" />
+        <input type="email" onChange={(e)=>setEmail(e.target.value)}  />
         <label>Password </label>
-        <input type="password" />
+        <input type="password" onChange={(e)=>setPassword(e.target.value)}   />
         <Link className="nav-link" to="/setting/register" input type="submit" value="Log In">Don't have An account?</Link>
         <Link className="nav-link" to="/setting/recoverPassword" input type="submit" value="Log In">Forget your password?</Link>
         <input type="submit" value="Log In"></input>
       </form>
-      </div>  
+        
+      </div> 
+      }
+      </> 
      );
 }
  
