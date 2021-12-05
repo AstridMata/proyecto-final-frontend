234c520677c73368015ca61c6e3288039825562d
AstridMata <mata.astrid.01@gmail.com>
2021-12-05 09:58:59 +0000

    button logout y correcciones

--- src/store/flux.js
+++ src/store/flux.js
@@ -1,50 +1,61 @@
+import axios from "axios";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
+
 		store: {
 			books: [],
-
-			kidsBooks :[]
+			auth: false,
+			kidsBooks: []
 		},
 
 		actions: {
-		    
+//peticion de libros
 			loadBooks: () => {
-            fetch(
-				`https://www.googleapis.com/books/v1/volumes?q=kids&orderBy=newest&key=AIzaSyAg-smCGbo7V5vB3tjlw_qOqbtIXwpykqg`
-			)
-			.then((response) => response.json())
-          .then((data) => setStore({ books: data.items }));
-  },
-
-			// Usa getActions para llamar una function dentro de una fuction. Te dejo un ejemplo
-			exampleFunction: () => {
-				getActions().changeColor("green");
+				fetch(
+					`https://www.googleapis.com/books/v1/volumes?q=kids&orderBy=newest&key=AIzaSyAg-smCGbo7V5vB3tjlw_qOqbtIXwpykqg`
+				)
+					.then((response) => response.json())
+					.then((data) => setStore({ books: data.items }));
 			},
-            //funcion de ejemplo para hacer peticiones fetch
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ demo: data.bar }))
-				*/
-			},
+//peticion libros infantiles con mas detalles
 			loadKidsBooks: () => {
 				fetch(`https://www.googleapis.com/books/v1/volumes?q=kids&orderBy=newest&key=AIzaSyAg-smCGbo7V5vB3tjlw_qOqbtIXwpykqg`)
-				.then(response => response.json())
-				.then(data => setStore({kidsBooks:data.items}));
-				
+					.then(response => response.json())
+					.then(data => setStore({ kidsBooks: data.items }));
+
 			},
-			changeColor: (color) => {
-                //actualiza un dato de la global store
-                setStore({ demo: color });
-                
-				//Para obtener datos de la store y usarlos como en el ejemplo de la linea 23 a la 26
-				// const store = getStore();
+//boton para desloguearse.
+			logout: () => {
+				localStorage.removeItem('token');
+				setStore({ auth: false })
+			},
+//accion para formulario login
+			login: (email, password) => {
 
+				axios.post('http://localhost:8080/setting/login', {
+					email: email,
+					password: password
+				})
+					.then(function (response) {
+						if (response.status === 200) {
+							setStore({ auth: true })
+							localStorage.setItem('token', response.data.token)
+						}
+						console.log(response);
+					})
+					.catch(function (error) {
+						console.log(error);
+					});
 
 			}
 
-			}
+
 		}
-	};
+
+	}
+}
+	
 
 
 export default getState;
