e32d9fd80c2f783bcaab6c83f16c282728e037c5
Judycampk <judhycampk71@outlook.com>
2021-12-08 15:47:24 -0600

    solucionando errores

--- src/views/Home.js
+++ src/views/Home.js
@@ -21,19 +21,19 @@ const Home = () => {
       <>
         <div className="row gradient">
         <div className="container border-0 mt-5 mb-5 ms-4 upcoming">
-          <h2 className="text-danger fw-bold">Romance Books</h2>
+          <h2 className="text-danger fw-bold">Romance-Books</h2>
             <RomanceBooks /> 
           </div>
         <div className="container border-0 mt-5 mb-5 ms-4 upcoming">
-          <h2 className="text-danger fw-bold">Thriller Books</h2>
+          <h2 className="text-danger fw-bold">Thriller-Books</h2>
             <ThrillerBooks /> 
           </div>
           <div className="container border-0 mt-5 mb-5 ms-4 upcoming">
-          <h2 className="text-danger fw-bold">Fantasy Books</h2>
+          <h2 className="text-danger fw-bold">Fantasy-Books</h2>
             <FantasyBooks />
           </div>
           <div className="container border-0 mt-5 mb-5 ms-4 upcoming">
-            <h2 className="text-danger fw-bold">Kids Books</h2>
+            <h2 className="text-danger fw-bold">Kids-Books</h2>
             <KidsBooks />
           </div>
         </div>
